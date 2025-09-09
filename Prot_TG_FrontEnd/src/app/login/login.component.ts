1d87be04642792247da2d6587ab8e00446483a8e
eduardogomesDev21 <eduardogmsnishida1@gmail.com>
2025-09-09 13:59:08 -0300

    Faz integração do login com o back

--- Prot_TG_FrontEnd/src/app/login/login.component.ts
+++ Prot_TG_FrontEnd/src/app/login/login.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, HttpClientModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css'],
+  encapsulation: ViewEncapsulation.None // 🔑 permite estilizar body e aplicar reset global só nesse componente
 })
 export class LoginComponent {
+  credenciais = {
+    email: '',
+    senha: ''
+  };
 
+  mensagem = '';
+  erro = '';
+
+  constructor(private http: HttpClient) {}
+
+  login(): void {
+    this.mensagem = '';
+    this.erro = '';
+
+    this.http.post('http://localhost:5000/login', this.credenciais).subscribe({
+      next: (res: any) => {
+        this.mensagem = res.message || 'Login realizado com sucesso!';
+        // Exemplo: salvar token no localStorage e redirecionar
+        // localStorage.setItem('token', res.token);
+      },
+      error: (err) => {
+        this.erro = err.error?.error || 'Erro ao realizar login';
+      }
+    });
+  }
 }
