22b2c2d6b91cd352b1dda7e2842391aaec632703
Caio Lima <caiolimacruz@gmail.com>
2025-09-09 15:43:09 -0300

    consertando visual de Login, estava tendo imprevisto com o back

--- Prot_TG_FrontEnd/src/app/app.module.ts
+++ Prot_TG_FrontEnd/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 @NgModule({
   imports: [
     BrowserModule,
