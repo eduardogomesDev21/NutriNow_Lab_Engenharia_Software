c17377850fa2b3cc5c75f16ef7be85143f1550b7
eduardogomesDev21 <eduardogmsnishida1@gmail.com>
2025-09-19 14:00:18 -0300

    Gera o componente Esqueci-Senha(componente pronto e funcionando com back)

--- Prot_TG_FrontEnd/src/app/app.routes.ts
+++ Prot_TG_FrontEnd/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { LoginComponent } from './login/login.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { HomeComponent } from './home/home.component';
 import { ChatbotComponent } from './chatbot/chatbot.component';
+import { EsqueciSenhaComponent } from './esqueci-senha/esqueci-senha.component';
 
 export const routes: Routes = [
     {path: 'login', component: LoginComponent},
     {path: 'cadastro', component: CadastroComponent},
     {path: 'home', component: HomeComponent},
     {path: 'chatbot', component: ChatbotComponent},
+    {path: 'esqueci-senha', component: EsqueciSenhaComponent},
     { path: '', redirectTo: 'home', pathMatch: 'full' } // testando essa parada
 ];
